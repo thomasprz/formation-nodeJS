c58e06ae48ab5f03306fdc0c9a44a6e4074043d5
Thomas <przegralek.thomas@gmail.com>
2025-04-19 23:34:43 +0200

    feat: ajout des notions de base sur les variables

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+//VARIABLES
+const lastName = 'Max';       // Constante, ne peut pas être réassignée
+let age = 29;                 // Variable pouvant être modifiée
+const hasHobbies = true;      // Booléen stocké dans une constante
+
+age = 30;                     // Mise à jour de la variable 'age'
